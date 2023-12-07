2a3766be76aae1c2615bf5c25a2b720e47611146
khuongdev2001 <khuongmy1@gmail.com>
2023-12-07 22:59:01 +0700

    add example

--- debouce_and_throttle/app.js
+++ debouce_and_throttle/app.js
@@ -1,13 +1,17 @@
 const search = document.querySelector("#search");
 const outputNormal = document.querySelector("#output_normal");
 const outputDebouce = document.querySelector("#output_debouce");
-search.addEventListener("input", function (e) {
-    outputNormal.textContent = e.target.value;
+const outputThrottle = document.querySelector("#output_throttle");
+search.addEventListener("input", function(e) {
+    outputNormal.textContent = `Normal: ${e.target.value}`;
 });
-search.addEventListener("input", debouce(function (e) {
-    outputDebouce.textContent = e.target.value;
-}, 500))
+search.addEventListener("input", debouce(function(e) {
+    outputDebouce.textContent = `Debouce ${e.target.value}`;
+}, 1000))
 
+search.addEventListener("input", throttle(function(e) {
+    outputThrottle.textContent = `Throttle ${e.target.value}`;
+}, 500));
 
 function debouce(callback, time) {
     let timer = null;
@@ -16,5 +20,17 @@ function debouce(callback, time) {
         timer = setTimeout(() => {
             callback(...args)
         }, time)
+    }
+}
+
+
+function throttle(callback, timer) {
+    let timeFlag = null;
+    return (...arg) => {
+        if (timeFlag) return false;
+        timeFlag = setTimeout(() => {
+            callback(...arg);
+            timeFlag = null;
+        }, timer);
     }
 }
